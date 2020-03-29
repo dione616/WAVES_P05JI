6667ffcb80fe322a20bbaf0b31a1633065389ecb
dione616 <Vetal160199@i.ua>
2020-03-29 19:38:09 +0300

    Added Auth Logout Admin Brand Wood Product

--- server/server.js
+++ server/server.js
@@ -13,12 +13,97 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-//MODELS
-const { User } = require("./model/user")
+//============================
+//      MODELS
+//============================
+
+const { User } = require("./models/user")
+const { Brand } = require("./models/brand")
+const { Wood } = require("./models/wood")
+const { Product } = require("./models/product.js")
+
 //============================
 //       MIDDLEWARES
 //============================
+
 const { auth } = require("./middleware/auth")
+const { admin } = require("./middleware/admin")
+
+//============================
+//      PRODUCTS
+//============================
+
+app.get("/api/product/articles_by_id", (req, res) => {
+  let type = req.query.type //query comes from bodyParser.urlencoded
+  let items = req.query.id
+
+  if (type === "array") {
+    let ids = req.query.id.split(",") // array of ids
+    items = [] //convert to []
+    items = ids.map(item => {
+      return mongoose.Types.ObjectId(item)
+    })
+  }
+
+  Product.find({ _id: { $in: items } })
+    .populate("brand")
+    .populate("wood")
+    .exec((err, docs) => {
+      return res.status(200).send(docs)
+    })
+})
+
+app.post("/api/product/article", auth, admin, (req, res) => {
+  const product = new Product(req.body)
+
+  product.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    res.status(200).json({ success: true, article: doc })
+  })
+})
+
+//============================
+//      WOODS
+//============================
+
+app.post("/api/product/wood", auth, admin, (req, res) => {
+  const wood = new Wood(req.body)
+
+  wood.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    res.status(200).json({ success: true, wood: doc })
+  })
+})
+
+app.get("/api/product/woods", (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if (err) return res.status(400).send(err)
+    res.status(200).send(woods)
+  })
+})
+
+//============================
+//      BRAND
+//============================
+
+app.post("/api/product/brand", auth, admin, (req, res) => {
+  const brand = new Brand(req.body)
+
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    res.status(200).json({
+      success: true,
+      brand: doc
+    })
+  })
+})
+
+app.get("/api/product/brands", (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if (err) return res.status(400).send(err)
+    res.status(200).send(brands)
+  })
+})
 
 //============================
 //      USERS
@@ -63,6 +148,13 @@ app.post("/api/users/login", (req, res) => {
           })
       })
     })
+  })
+})
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).send({ success: true })
   })
 })
 
