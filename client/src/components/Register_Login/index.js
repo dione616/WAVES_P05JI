db226f8bdee5bf7aa039346cd5012b574dbbfa69
dione616 <Vetal160199@i.ua>
2020-03-31 16:42:29 +0300

    Install redux and RegLog component

---
+++ client/src/components/Register_Login/index.js
@@ -0,0 +1,41 @@
+import React from "react"
+import Button from "../utils/Button"
+
+const RegisterLogin = () => {
+  return (
+    <div className="page_wrapper">
+      <div className="container">
+        <div className="register_login_container">
+          <div className="left">
+            <h1>New Customer</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, blanditiis doloribus fuga veniam quae quod
+            illo recusandae rem quas ratione, eius, voluptate aspernatur praesentium veritatis saepe tempore dicta
+            voluptatem officia.
+            <Button
+              type="default"
+              title="Create an account"
+              linkTo="/register"
+              addStyle={{
+                margin: "15px"
+              }}
+            />
+          </div>
+          <div className="right">
+            <h1>Register customer</h1>
+            If you have an account please login
+            <Button
+              type="default"
+              title="Login"
+              linkTo="/login"
+              addStyle={{
+                margin: "10px"
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RegisterLogin
